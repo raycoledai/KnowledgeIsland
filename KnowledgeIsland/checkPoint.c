e3dcdc8393ad32c9dc8d4211b43cd29f9b61a6ec
acaciamok <acaciamok@hotmail.com>
2016-05-11 13:47:57 +1000

    Update checkPoint.c

--- KnowledgeIsland/checkPoint.c
+++ KnowledgeIsland/checkPoint.c
@@ -3,6 +3,9 @@
 #include <assert.h>
 #include <math.h>
 
+#define TRUE 1
+#define FLASE 0
+
 void testCheckPoint ();
 int checkPoint (int x, int y);
 
@@ -17,7 +20,7 @@ int main (int argc, char * argv[]) {
     
     int truePoint = checkPoint(xCor, yCor);
     
-    if (truePoint == 1) {
+    if (truePoint == TRUE) {
         printf("It is in the graph!\n");
     } else {
         printf("It is outside of the graph!\n");
@@ -25,51 +28,51 @@ int main (int argc, char * argv[]) {
 }
 
 void testCheckPoint () {
-    assert (checkPoint (0, 1) == 1);
-    assert (checkPoint (-4, 1) == 1);
-    assert (checkPoint (-5, -1) == 1);
-    assert (checkPoint (3, 5) == 1);
-    assert (checkPoint (4, -3) == 1);
-    assert (checkPoint (1, 7) == 0);
-    assert (checkPoint (-2, -7) == 0);
-    assert (checkPoint (6, -8) == 0);
-    assert (checkPoint (-8, 6) == 0);
+    assert (checkPoint (0, 1) == TRUE);
+    assert (checkPoint (-4, 1) == TRUE);
+    assert (checkPoint (-5, -1) == TRUE);
+    assert (checkPoint (3, 5) == TRUE);
+    assert (checkPoint (4, -3) == TRUE);
+    assert (checkPoint (1, 7) == FALSE);
+    assert (checkPoint (-2, -7) == FALSE);
+    assert (checkPoint (6, -8) == FALSE);
+    assert (checkPoint (-8, 6) == FALSE);
 }
 
 int checkPoint (int x, int y){
-    int point = 0;
+    int point = FALSE;
 
     //y-coordinate needs to be odd
     if (y%2 == 1) {
-        point = 1;
+        point = TRUE;
     } else {
-        point = 0;
+        point = FALSE;
     }
     
     //max region is 5x5
     if (abs(x)>5 || abs(y)>5) {
-        point = 0;
+        point = FALSE;
     }
     
     //in the region 3x5, its a point
     if (abs(x) <=3 && abs(y) <=5) {
-        point = 1;
+        point = TRUE;
     }
     
     //other regions
     if (abs(x) > 3 && abs(x) <= 4) {
         if (abs(y) <= 3) {
-            point = 1;
+            point = TRUE;
         } else {
-            point = 0;
+            point = FALSE;
         }
     }
     
     if (abs(x) > 4 && abs(x) <= 5) {
         if (abs(y) <= 1) {
-            point = 1;
+            point = TRUE;
         } else {
-            point = 0;
+            point = FALSE;
         }
     }
     return point;
